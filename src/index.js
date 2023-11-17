eedd17ffb0f2869b4f105f4333e1db6aa9c966f7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-11-17 08:55:26 -0600

    feat: introduce react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPage from './pages/Landing/LandingPage';
+import SchedulingPage from './pages/Scheduling/SchedulingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />
+  },
+  {
+    path: '/:location',
+    element: <SchedulingPage />
+  }
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
